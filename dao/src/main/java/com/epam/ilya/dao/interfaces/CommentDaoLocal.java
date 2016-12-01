a6d36f52ffc6302d9eb9a078f3c1b47427635edb
BondarenkoIlya <bondarenko_elijah@mail.ru>
2016-12-01 11:11:16 +0600

    #21 Add comment's entities

---
+++ dao/src/main/java/com/epam/ilya/dao/interfaces/CommentDaoLocal.java
@@ -0,0 +1,6 @@
+package com.epam.ilya.dao.interfaces;
+
+import com.epam.ilya.domain.entities.Comment;
+
+public interface CommentDaoLocal extends Dao<Comment> {
+}
---
+++ dao/src/main/java/com/epam/ilya/dao/services/CommentDaoJPA.java
@@ -0,0 +1,36 @@
+package com.epam.ilya.dao.services;
+
+import com.epam.ilya.dao.exceptions.DaoException;
+import com.epam.ilya.dao.interfaces.CommentDaoLocal;
+import com.epam.ilya.domain.entities.Comment;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+public class CommentDaoJPA implements CommentDaoLocal {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public Comment create(Comment entity) throws DaoException {
+        manager.persist(entity);
+        manager.flush();
+        return entity;
+    }
+
+    @Override
+    public Comment findById(Long id) throws DaoException {
+        return manager.find(Comment.class, id);
+    }
+
+    @Override
+    public Comment update(Comment entity) throws DaoException {
+        return manager.merge(entity);
+    }
+
+    @Override
+    public void delete(Comment entity) throws DaoException {
+        manager.remove(entity);
+    }
+}
