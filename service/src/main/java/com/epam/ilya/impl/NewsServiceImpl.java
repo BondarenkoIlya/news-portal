cf27ed48e439422b2a55dd1db1428007d9a78228
BondarenkoIlya <bondarenko_elijah@mail.ru>
2016-12-02 17:27:55 +0600

    #26 Implement methods

---
+++ service/src/main/java/com/epam/ilya/impl/NewsServiceImpl.java
@@ -0,0 +1,60 @@
+package com.epam.ilya.impl;
+
+import com.epam.ilya.dao.api.CommentDaoLocal;
+import com.epam.ilya.dao.api.NewsDaoLocal;
+import com.epam.ilya.domain.entities.Comment;
+import com.epam.ilya.domain.entities.News;
+import com.epam.ilya.api.NewsService;
+
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import java.util.List;
+
+@Stateless
+@Default
+public class NewsServiceImpl implements NewsService {
+
+
+    private NewsDaoLocal newsDaoLocal;
+
+
+    private CommentDaoLocal commentDaoLocal;
+
+    @Inject
+    public NewsServiceImpl(NewsDaoLocal newsDaoLocal, CommentDaoLocal commentDaoLocal) {
+        this.newsDaoLocal = newsDaoLocal;
+        this.commentDaoLocal = commentDaoLocal;
+    }
+
+
+    @Override
+    public News findById(long id) {
+        return newsDaoLocal.findById(id);
+    }
+
+    @Override
+    public List<News> getAllNews() {
+        return newsDaoLocal.findAll();
+    }
+
+    @Override
+    public void deleteNews(News news) {
+        newsDaoLocal.delete(news);
+    }
+
+    @Override
+    public News updateNews(News news) {
+        return newsDaoLocal.update(news);
+    }
+
+    @Override
+    public void createNews(News news) {
+        newsDaoLocal.create(news);
+    }
+
+    @Override
+    public void deleteComment(Comment comment) {
+        commentDaoLocal.delete(comment);
+    }
+}
--- service/src/main/java/com/epam/ilya/implementations/NewsServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package com.epam.ilya.implementations;
-
-import com.epam.ilya.interfaces.NewsService;
-
-public class NewsServiceImpl implements NewsService {
-}
