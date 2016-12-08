35bd81f31d65815f1a6cbb2abe5227b544774b36
BondarenkoIlya <bondarenko_elijah@mail.ru>
2016-12-08 17:50:35 +0600

    #32 Refactor service methods

--- service/src/main/java/com/epam/ilya/api/NewsService.java
+++ service/src/main/java/com/epam/ilya/api/NewsService.java
@@ -2,21 +2,22 @@ package com.epam.ilya.api;
 
 import com.epam.ilya.domain.entities.Comment;
 import com.epam.ilya.domain.entities.News;
+import com.epam.ilya.exception.ServiceException;
 
 import java.util.List;
 
 public interface NewsService {
-    News findById(long id);
+    News findById(long id) throws ServiceException;
 
     List<News> getAllNews();
 
     void deleteNews(News news);
 
-    News updateNews(News news);
+    News updateNews(News news) throws ServiceException;
 
-    void createNews(News news);
+    void createNews(News news) throws ServiceException;
 
-    void createComment(Comment comment);
+    News createCommentForNews(Comment comment,News news) throws ServiceException;
 
-    void deleteComment(Comment comment);
+    News deleteCommentFromNews(Comment comment,News news) throws ServiceException;
 }
--- service/src/main/java/com/epam/ilya/impl/NewsServiceImpl.java
+++ service/src/main/java/com/epam/ilya/impl/NewsServiceImpl.java
@@ -3,11 +3,14 @@ package com.epam.ilya.impl;
 import com.epam.ilya.api.NewsService;
 import com.epam.ilya.dao.api.CommentDaoLocal;
 import com.epam.ilya.dao.api.NewsDaoLocal;
+import com.epam.ilya.dao.exceptions.DaoException;
 import com.epam.ilya.domain.entities.Comment;
 import com.epam.ilya.domain.entities.News;
+import com.epam.ilya.exception.ServiceException;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.inject.Named;
 import java.util.List;
 
 @Stateless
@@ -27,8 +30,12 @@ public class NewsServiceImpl implements NewsService {
 
 
     @Override
-    public News findById(long id) {
-        return newsDaoLocal.findById(id);
+    public News findById(long id) throws ServiceException {
+        try {
+            return newsDaoLocal.findById(id);
+        } catch (DaoException e) {
+            throw new ServiceException("Cannot find news with id" + id, e);
+        }
     }
 
     @Override
@@ -42,22 +49,47 @@ public class NewsServiceImpl implements NewsService {
     }
 
     @Override
-    public News updateNews(News news) {
+    public News updateNews(News news) throws ServiceException {
+        try {
+            newsDaoLocal.findById(news.getId());
+        } catch (DaoException e) {
+            throw new ServiceException("Cannot update nonexistent news",e);
+        }
         return newsDaoLocal.update(news);
     }
 
     @Override
-    public void createNews(News news) {
-        newsDaoLocal.create(news);
+    public void createNews(News news) throws ServiceException {
+        try {
+            newsDaoLocal.create(news);
+        } catch (DaoException e) {
+            throw new ServiceException("Cannot create news",e);
+        }
     }
 
     @Override
-    public void createComment(Comment comment) {
-        commentDaoLocal.create(comment);
+    public News createCommentForNews(Comment comment, News news) throws ServiceException {
+        News newNews;
+        try {
+            comment.setNewsId(news.getId());
+            commentDaoLocal.create(comment);
+            newNews = newsDaoLocal.findById(news.getId());
+        } catch (DaoException e) {
+            throw new ServiceException("Cannot find comment's news. News doesn't exist",e);
+        }
+        return newNews;
     }
 
     @Override
-    public void deleteComment(Comment comment) {
+    public News deleteCommentFromNews(Comment comment, News news) throws ServiceException {
+        News updatedNews;
+        news.getComments().remove(comment);
         commentDaoLocal.delete(comment);
+        try {
+           updatedNews= newsDaoLocal.findById(news.getId());
+        } catch (DaoException e) {
+            throw new ServiceException("Cannot find deleting comment's news",e);
+        }
+        return updatedNews;
     }
 }
