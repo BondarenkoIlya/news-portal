70e2877cfb063245a91fb334dddfdfcd3dd81c70
BondarenkoIlya <bondarenko_elijah@mail.ru>
2016-12-27 14:55:20 +0600

    #56 Add mapped user and user's role entities

---
+++ domain/src/main/java/com/epam/ilya/domain/entities/User.java
@@ -0,0 +1,60 @@
+package com.epam.ilya.domain.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = -2692253546761426748L;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "enable")
+    private boolean enabled;
+
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<UserRole> userRole = new HashSet<>(0);
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<UserRole> getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(Set<UserRole> userRole) {
+        this.userRole = userRole;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
