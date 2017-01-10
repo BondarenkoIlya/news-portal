b7ba73a26f9aaa73439cb870e3d00b9272fc619d
BondarenkoIlya <bondarenko_elijah@mail.ru>
2017-01-10 14:07:44 +0600

    #59 Configured access for all user's roles

--- service/src/main/java/com/epam/ilya/config/WebSecurityConfig.java
+++ service/src/main/java/com/epam/ilya/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 @Configuration
 @ComponentScan({"com.epam.ilya.security"})
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -28,16 +30,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                //.antMatchers("/home.xhtml").hasAnyRole("USER")
-                .anyRequest().hasAnyRole("USER")
-                .and()
+                .antMatchers("/pages/*", "/*").permitAll()
+                .antMatchers("/pages/user/**").hasAnyRole("USER")
+                .antMatchers("/pages/admin/**").hasAnyRole("ADMIN")
+                    .and()
                 .formLogin()
-                    .loginPage("/login.xhtml").permitAll()
-                    .loginProcessingUrl("/login").permitAll()
-                    .successForwardUrl("/home.xhtml")
-                .and()
+                .loginPage("/login.xhtml")
+                .loginProcessingUrl("/login")
+                .successForwardUrl("/pages/home.xhtml")
+                    .and()
+                .logout()
+                .logoutUrl("/logout").permitAll()
+                .logoutSuccessUrl("/login.xhtml")
+                .invalidateHttpSession(true)
+                    .and()
                 .httpBasic()
-                .and()
+                    .and()
                 .csrf().disable();
     }
 }
